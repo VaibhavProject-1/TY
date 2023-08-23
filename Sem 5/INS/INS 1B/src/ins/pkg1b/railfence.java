a74925c092b00bd4bc2f8b47116547da1f1b206a
VaibhavProject-1 <vaibhavpatel003@gmail.com>
2023-08-23 23:27:25 +0530

    added INS folder
    Added first five practicals from Information and Network Security

---
+++ Sem 5/INS/INS 1B/src/ins/pkg1b/railfence.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ins.pkg1b;
+
+/**
+ *
+ * @author 91885
+ */
+
+
+public class railfence {
+public static void main(String args[])
+{
+String input = "ismile";
+String output = "";
+int len = input.length(),flag = 0;
+System.out.println("Input String : " + input);
+for(int i=0;i<len;i+=2) {
+    output += input.charAt(i);
+}
+for(int i=1;i<len;i+=2) {
+    output += input.charAt(i);
+}
+    System.out.println("Ciphered Text : "+output);
+}
+}
