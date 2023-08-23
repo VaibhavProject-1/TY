a87cd36cd7586e6a01899018890024538e714479
VaibhavProject-1 <vaibhavpatel003@gmail.com>
2023-08-23 23:29:42 +0530

    added Practical 1B
    Added Practical with image frame draw

---
+++ Sem 5/GP/P1B/P1B/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using WindowsFormsApplication10;
+
+namespace P1B
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
