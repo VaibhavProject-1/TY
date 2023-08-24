ee3f8b7fbfcc8ac708cefc976a91fc90785531e0
VaibhavProject-1 <vaibhavpatel003@gmail.com>
2023-08-24 14:22:53 +0530

    Added triangle drawing practical

---
+++ Sem 5/GP/Prac triangle/Prac triangle/Program.cs
@@ -0,0 +1,22 @@
+﻿using DxPrac1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Prac_triangle
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
